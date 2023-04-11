4476db207f59b92ee547c8c7cd3f41e8d15358b4
jayant <ja@trinityagtech.com>
2023-04-11 12:00:26 +0530

    removed orm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { config } from 'ormcofig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -8,7 +7,12 @@ import { QuizModule } from './quiz/quiz.module';
 import { UserModule } from './users/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config), UserModule, QuizModule],
+  imports: [TypeOrmModule.forRoot({
+    type:'sqlite',
+    database:'db',
+    entities:['dist/src/**/*.entity.js'],
+    synchronize:true,
+}), UserModule, QuizModule],
   controllers: [AppController],
   providers: [AppService],
 })
